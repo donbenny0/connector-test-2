ff844fa7aeb05d3c5c4dd517526baa76b2ce9f6b
don benny <don.benny@ayatacommerce.com>
2024-09-27 17:02:20 +0530

    test event added

--- event/src/controllers/event.controller.ts
+++ event/src/controllers/event.controller.ts
@@ -12,55 +12,10 @@ import { logger } from '../utils/logger.utils';
  * @returns
  */
 export const post = async (request: Request, response: Response) => {
-  let customerId = undefined;
-
-  // Check request body
-  if (!request.body) {
-    logger.error('Missing request body.');
-    throw new CustomError(400, 'Bad request: No Pub/Sub message was received');
-  }
-
-  // Check if the body comes in a message
-  if (!request.body.message) {
-    logger.error('Missing body message');
-    throw new CustomError(400, 'Bad request: Wrong No Pub/Sub message format');
-  }
-
-  // Receive the Pub/Sub message
-  const pubSubMessage = request.body.message;
-
-  // For our example we will use the customer id as a var
-  // and the query the commercetools sdk with that info
-  const decodedData = pubSubMessage.data
-    ? Buffer.from(pubSubMessage.data, 'base64').toString().trim()
-    : undefined;
-
-  if (decodedData) {
-    const jsonData = JSON.parse(decodedData);
-
-    customerId = jsonData.customer.id;
-  }
-
-  if (!customerId) {
-    throw new CustomError(
-      400,
-      'Bad request: No customer id in the Pub/Sub message'
-    );
-  }
-
   try {
-    const customer = await createApiRoot()
-      .customers()
-      .withId({ ID: Buffer.from(customerId).toString() })
-      .get()
-      .execute();
-
-    // Execute the tasks in need
-    logger.info(customer);
+    response.json({ "message": request.body })
   } catch (error) {
-    throw new CustomError(400, `Bad request: ${error}`);
+    response.status(500);
+    response.json({ "error": error })
   }
-
-  // Return the response for the client
-  response.status(204).send();
 };
